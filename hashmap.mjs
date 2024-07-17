5a369d0f89d0f9ceaefd0b034e67872fa80e61e4
Ethan Farber <edf1247@gmail.com>
2024-07-17 14:24:37 -0400

    added set function, currently it handles collisions, need to add resize

---
+++ hashmap.mjs
@@ -0,0 +1,43 @@
+import {LinkedList} from '../linkedlist/linkedlist.mjs'
+
+class HashMap {
+
+    constructor(){
+        this.capacity = 16;
+        this.loadfactor = .75;
+        this.array = Array(this.capacity);
+    }
+
+    hash(key) {
+        let constant = .69;
+        let table_size = this.capacity;
+        let hashcode = 0;
+        for(let i = 0; i < key.length; i++){
+            hashcode += Math.floor(table_size * (key.charCodeAt(i) % constant));
+        }
+
+        return hashcode;
+    }
+
+    set(key, value){
+        let bucket = this.hash(key) % this.capacity;
+        //creating linked list in case of collision
+        if(this.array[bucket] != null && this.array[bucket][0] != key){
+            let list = new LinkedList();
+            list.append(this.array[bucket]);
+            list.append(key, value);
+        }
+        else {
+            this.array[bucket] = [key, value];
+        }
+
+    }
+    
+}
+
+let hash = new HashMap();
+hash.set("test", "apple");
+hash.set("test", "bannana");
+hash.set("fruit", "apple");
+
+console.log(hash);
